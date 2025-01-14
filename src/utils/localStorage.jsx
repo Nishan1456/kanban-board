e0e2787be27b2c0733dcdc66783221552606b007
suraz <suraz@surazs-Laptop.local>
2025-01-14 15:18:08 +0545

    Completed

--- src/utils/localStorage.jsx
+++
@@ -1,9 +0,0 @@
-// src/utils/localStorage.js
-export const saveToLocalStorage = (key, value) => {
-  localStorage.setItem(key, JSON.stringify(value));
-};
-
-export const loadFromLocalStorage = (key, defaultValue) => {
-  const storedValue = localStorage.getItem(key);
-  return storedValue ? JSON.parse(storedValue) : defaultValue;
-};
